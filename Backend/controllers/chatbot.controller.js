cc1645d2dfdcfc6507c3730d77a235813a04c916
Anupam <9582anupamk@gmail.com>
2025-02-28 03:08:48 +0530

    added assembly api

--- Backend/controllers/chatbot.controller.js
+++ Backend/controllers/chatbot.controller.js
@@ -1,5 +1,6 @@
 import { generateBotResponse } from "../services/chatbot.service.js";
 import { fetchYouTubeAudio } from "../helper/fetchYouTubeAudio.js";
+import { transcribeAudio } from "../helper/transcribeAudio.js";
 
 
 export const getBotResponse = async (req, res) => {
@@ -8,19 +9,19 @@ export const getBotResponse = async (req, res) => {
         res.status(200).json({ response });
     } catch (error) {
         console.error('Error in chatbot response:', error);
-        res.status(500).json({ 
-            message: 'Error generating response' 
+        res.status(500).json({
+            message: 'Error generating response'
         });
     }
 };
 
 export const ytToAudio = async (req, res) => {
     try {
-        const { videoUrl } = req.body;
-        // const videoUrl = "https://www.youtube.com/watch?v=JC82Il2cjqA";
+        // const { videoUrl } = req.body;
+        const videoUrl = "https://www.youtube.com/watch?v=JC82Il2cjqA";
         if (!videoUrl) {
-            return res.status(400).json({ 
-                message: 'Missing video URL' 
+            return res.status(400).json({
+                message: 'Missing video URL'
             });
         }
 
@@ -28,9 +29,32 @@ export const ytToAudio = async (req, res) => {
         res.status(200).json({ filePath });
     } catch (error) {
         console.error('Error fetching YouTube audio:', error);
-        res.status(500).json({ 
-            message: 'Error fetching YouTube audio' 
+        res.status(500).json({
+            message: 'Error fetching YouTube audio'
         });
     }
 };
 
+
+
+export const audioToTranscript = async (req, res) => {
+    try {
+        const { filePath } = req.body;
+        console.log('filePath:', req.body);
+        // const filePath = "/path/to/your/audio/file.wav";
+        if (!filePath) {
+            return res.status(400).json({
+                message: 'Missing audio file path'
+            });
+        }
+
+        const transcript = await transcribeAudio(filePath);
+        res.status(200).json({ transcript });
+    }
+    catch (error) {
+        console.error('Error transcribing audio:', error);
+        res.status(500).json({
+            message: 'Error transcribing audio'
+        });
+    }
+};
--- Backend/routes/v1/chatbot.routes.js
+++ Backend/routes/v1/chatbot.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getBotResponse, ytToAudio } from "../../controllers/chatbot.controller.js";
+import { getBotResponse, ytToAudio, audioToTranscript } from "../../controllers/chatbot.controller.js";
 
 const router = express.Router();
 
 router.post("/bot-response", getBotResponse);
 router.post("/yt-to-audio", ytToAudio);
+router.post("/audio-to-transcript", audioToTranscript);
 
 export default router;
